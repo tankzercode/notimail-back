0500060f72fe4de544adce26524e5026e2603e09
tanguy <tlegalldutertre@gmail.com>
2023-12-15 10:23:03 +0100

    creation des routes et de l'api

--- routes/users.js
+++ routes/users.js
@@ -56,7 +56,7 @@ router.get('/all', async (req, res) => {
 router.get('/users', async (req, res) => {
   try {
     const users = await User.findAll({
-      attributes: ['firm_name', "email", "last_name", "phone_number", "has_mail", "is_admin"]
+      attributes: ['firm_name', "email", "last_name", "phone_number", "has_mail", "is_admin", "id"]
     })
 
     res.status(200).send(users)
@@ -120,16 +120,16 @@ router.get('/isAuth', auth, (req, res) => {
 router.post('/', async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
+    const password = await bcrypt.hash(req.body.code, salt)
     const user = await User.create({
       first_name: req.body.first_name,
       last_name: req.body.last_name,
       firm_name: req.body.firm_name,
       email: req.body.email,
       phone_number: req.body.phone_number,
-      password: await bcrypt.hash(req.body.password, salt),
+      password: password,
       is_admin: req.body.is_admin,
     });
-    user.password = ""
 
     res.status(200).send("utilisateur ajouté")
   } catch (error) {
@@ -181,6 +181,40 @@ router.put('/:firmName', async (req, res) => {
     res.status(401).send(error)
 
   }
+})
+
+
+router.patch("/", async (req, res) => {
+try {
+  console.log(req.body)
+ await User.update({ has_mail : true },{ where : { firm_name : req.body }}); 
+
+
+
+//  envoyer Mail
+ res.status(200).send("notification envoyé")
+  
+} catch (error) {
+
+  res.status(401).send(error)
+  
+}
+})
+
+router.patch('/:firmName',async (req,res)=> {
+try {
+  await User.update({ has_mail : false },{ where : { firm_name : req.params.firmName }}); 
+  const isUpdate = await User.findOne({ where: { firm_name: req.params.firmName } });
+  isUpdate.password = null
+
+  res.status(200).send(isUpdate)
+
+
+} catch (error) {
+  console.log(error)
+  res.status(401).send(error)
+
+}
 })
 
 module.exports = router;
